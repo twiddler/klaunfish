24064f8c8ed4ccc3b17a6c058ba13349474bf758
twiddler <13364827+twiddler@users.noreply.github.com>
2021-01-22 04:58:16 +0100

    Basic prompt

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 import chess
+from colorama import Fore
+from colorama import Style
+from colorama import Back
+
+def error(message):
+    print(f"{Fore.RED}{message}{Style.RESET_ALL}")
+
+str_white = f"{Back.WHITE}{Fore.BLACK} white {Style.RESET_ALL}"
+
+if __name__ == "__main__":
+    board = chess.Board()
+
+    while(True):
+        uci = input(f"Which move to play for {str_white}? ")
+
+        try:
+            board.push_san(uci)
+            print(board)
+        except ValueError:
+            error("Not a (legal) move. Try again!\n")
+
+    
+    print(bool(board.castling_rights))

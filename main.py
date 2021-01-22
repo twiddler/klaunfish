9ca921499c7076c0a115cca2c01dce1b7b42ca2d
twiddler <13364827+twiddler@users.noreply.github.com>
2021-01-22 05:45:13 +0100

    ... this too

--- main.py
+++ main.py
@@ -3,22 +3,45 @@ from colorama import Fore
 from colorama import Style
 from colorama import Back
 
-def error(message):
+import negamax
+
+def error(message: str):
     print(f"{Fore.RED}{message}{Style.RESET_ALL}")
 
-str_white = f"{Back.WHITE}{Fore.BLACK} white {Style.RESET_ALL}"
+def info(message: str):
+    print(f"{Fore.YELLOW}{message}{Style.RESET_ALL}")
+
+def player_string(is_white):
+    if is_white:
+        return f"{Back.WHITE}{Fore.BLACK} white {Style.RESET_ALL}"
+    else:
+        return f"{Back.BLACK}{Fore.WHITE} black {Style.RESET_ALL}"
+
+def play_move_for_opponent(board, is_white):
+    uci = input(f"Which move to play for {player_string(is_white)}? ")
+    try:
+        board.push_san(uci)
+        print("")
+        print(board, "\n")
+    except ValueError:
+        error("Not a (legal) move. Try again!\n")
+        play_move_for_opponent(board, is_white)
 
 if __name__ == "__main__":
     board = chess.Board()
+    i_am_white = False
 
     while(True):
-        uci = input(f"Which move to play for {str_white}? ")
 
-        try:
-            board.push_san(uci)
-            print(board)
-        except ValueError:
-            error("Not a (legal) move. Try again!\n")
+        play_move_for_opponent(board, not i_am_white)
 
-    
-    print(bool(board.castling_rights))
+        print(f"Calculating best move for {player_string(i_am_white)} ...")
+        (move, rating) = negamax.best_move(board, i_am_white, 1)
+
+        if move == None:
+            info(f"No legal move left.")
+            exit(0)
+
+        board.push(move)
+        print("")
+        print(board, "\n")
